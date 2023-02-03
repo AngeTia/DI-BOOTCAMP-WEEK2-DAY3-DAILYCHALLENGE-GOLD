2d353242abc8526b31dcddcc4e8797bf1cf6ce49
Gompou Tia Gbongué Ange <35264040+AngeTia@users.noreply.github.com>
2023-02-03 10:45:02 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ const numbers = [5,0,9,1,7,4,2,6,3,8];
 
 let essai = numbers.toString();
 
-let essai1 = numbers.join(',');
-let essai2 = numbers.join('+');
-let essai3 = numbers.join(' ');
-let essai4 = numbers.join('');
+let firstTry = numbers.join(',');
+let secondTry = numbers.join('+');
+let thirdTry = numbers.join(' ');
+let forthTry = numbers.join('');
 
 let ordering = [];
 
 for (let i = 0; i <numbers.length; i++) {
-    let elemi = numbers[i];
+    let elementTest = numbers[i];
     for (let j = i; j <numbers.length; j++) {
-        let elemj = numbers[j]
-        if (elemi <= elemj){
-            elemi = elemj;
-            ordering[i]= elemi;
+        let elementStock = numbers[j]
+        if (elementTest <= elementStock){
+            elementTest = elementStock;
+            ordering[i]= elementTest;
         }else {
-            ordering[i]= elemi;
+            ordering[i]= elementTest;
         }
     }
     let index = numbers.indexOf(ordering[i]);
